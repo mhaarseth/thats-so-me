45e24fc81a0b17d806effdaa6cab32c312e9d682
mhaarseth <100208301+mhaarseth@users.noreply.github.com>
2023-10-15 21:57:54 +0200

    Added new post functionality

--- js/feed.js
+++ js/feed.js
@@ -50,3 +50,45 @@ async function getPosts(url) {
 const postsUrl = `${NOROFF_API}posts`;
 
 getPosts(postsUrl);
+
+const modalForm = document.getElementById("modalForm");
+// console.log(modalForm);
+
+modalForm.addEventListener("submit", newPost);
+
+async function newPost(event) {
+  event.preventDefault();
+
+  const formContent = new FormData(modalForm);
+  console.log(formContent);
+  const newPostContent = Object.fromEntries(formContent.entries());
+
+  try {
+    const response = await postNewPost(newPostContent);
+    window.location.href = "/feed/";
+  } catch (error) {
+    alert(error);
+  }
+}
+
+async function postNewPost(newPostContent) {
+  const newPostUrl = `${NOROFF_API}posts/`;
+  const token = localStorage.getItem("token");
+
+  const newPostOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(newPostContent),
+  };
+
+  const response = await fetch(newPostUrl, newPostOptions);
+  const json = await response.json();
+
+  if (response.ok === true) {
+    return json;
+  }
+  throw new Error(json.errors[0].message);
+}
